1731f0f54ad9787b1e053394147daceeaa71e913
mmantle-hud <m.e.mantle@hud.ac.uk>
2016-11-23 16:41:59 +0000

    initial commit

---
+++ xyz/login_process.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Login Process</title>
+</head>
+
+<body>
+
+<?php
+echo "Successfully logged in go to the <a href='page1.php'>homepage</a>"
+?>
+
+</body>
+</html>
