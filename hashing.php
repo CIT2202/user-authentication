51b1824be53a98c5340c9aae71693e03f15e5b9f
mmantle-hud <m.e.mantle@hud.ac.uk>
2020-11-03 11:56:24 +0000

    Update hashing.php

--- hashing.php
+++ hashing.php
@@ -13,6 +13,4 @@ echo "<p>brcypt hash of 'huddersfield' = {$hashOfhuddersfield}</p>";
 //the two hashes will be different the password_hash() algorithm adds a salt
 $hashOfhuddersfield = password_hash("huddersfield", PASSWORD_DEFAULT);
 echo "<p>brcypt hash of 'huddersfield' = {$hashOfhuddersfield}</p>";
-var_dump(password_verify('huddersfield','$2y$10$em6X15KQt4prqPeJ0g9Dg.2zLzhC/WKPrKpRfdHDw0VNUkH/FR9lK'));
-echo "hello"
 ;?>
