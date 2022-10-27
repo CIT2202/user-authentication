97ecfdf5b1f2f4ab3283e945b41d1e5e3f367556
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-10-27 10:50:51 +0100

    Update index.php

--- xyz/index.php
+++ xyz/index.php
@@ -24,7 +24,6 @@ if(!isset($_SESSION["user"]))
         <li><a href="page1.php">Page 1</a></li>
         <li><a href="page2.php">Page 2</a></li>
         <li><a href="page3.php">Page 3</a></li>
-        <li><a href="logout.php">Logout</a></li>
     </ul>
 </nav>
 
