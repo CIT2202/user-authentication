1a7645da696fd145243597698a2d4b71ff372f51
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-10-27 10:38:45 +0100

    Update index.php

--- xyz/index.php
+++ xyz/index.php
@@ -3,7 +3,7 @@ session_start();
 if(!isset($_SESSION["user"]))
 {
 	//user tried to access the page without logging in
-  //redirect them to the login page
+        //redirect them to the login page
 	header( "Location: login.php" );
 };
 ?>
